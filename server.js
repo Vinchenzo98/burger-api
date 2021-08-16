4ba3b2f81085acb964e2f3f5feab88427f6a6489
Vince <37967982+Vinchenzo98@users.noreply.github.com>
2021-08-16 17:30:20 +0100

    new

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 const theServer = require('json-server');
 const server = theServer.create();
 const router = theServer.router('db.json');
@@ -7,3 +8,14 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 server.listen(port);
+=======
+const theServer = require('json-server');
+const server = theServer.create();
+const router = theServer.router('db.json');
+const middlewares = theServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
+>>>>>>> Stashed changes
